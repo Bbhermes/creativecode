56b084037f99709d3ab2ce836f81cf7bbfeb2dc2
Bbhermes <54549417+Bbhermes@users.noreply.github.com>
2019-11-25 13:17:08 -0500

    Update MyNovel.py

--- novel/MyNovel.py
+++ novel/MyNovel.py
@@ -1,7 +1,11 @@
 !pip install markovify
+!pip install weasyprint
 
 from google.colab import files
 import markovify
+from weasyprint import HTML
+
+novel = ''
 
 uploaded = files.upload()
 for fn in uploaded.keys():
@@ -10,14 +14,24 @@ for fn in uploaded.keys():
 text_model = markovify.Text(text)
 
 
-f = open("MyNovel.txt", "w")
-
 for i in range(7000):
 
-  f.write(text_model.make_short_sentence(130))
+  novel += str(text_model.make_short_sentence(130))
 
+html_template = f"""
+<html>
+  <head>
+  <title>Troll2</title>
+  <style>
+    body {{
+      font-family: "Times New Roman";
+    }}
+  </style>
+  </head>
+  <body>
+  {novel}
+  </body>
+</html>
+"""
 
-f.close()
-
-f = open("MyNovel.txt", "r")
-print(f.read())
+HTML(string=html_template).write_pdf("MyNovel.pdf")
